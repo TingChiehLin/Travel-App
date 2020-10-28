40aec0c6aa332f1088fc7d515697bd187b064f7f
Jay Lin <cooloojayoo@gmail.com>
2020-10-28 23:06:30 +1100

    Travel App

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,39 +1,28 @@
-import {app} from '../src/server/server';
+import * as app from '../src/server/server';
 const request = require('supertest');
 
 describe("Testing server root", () => {
-    it("Return App method is defined", () => {
+
+    test("Return App method is defined", (done) => {
         expect(app).toBeDefined();
+        done();
     })
-   
-    it("Response the GET method", done => {
-        request(app)
-            .get("/")
-            .then(response => {
-                expect(response.statusCode).toBe(200);
-                done();
-            });
+});
+
+describe("GET Method", () => {
+    it('should return a 200', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
     });
-    it("Response the POST method", async (done) => {
+});
+
+describe('post /api/v1/pets', () => {
+    test("Response the POST method", async () => {
         const res = await request(app)
             .post('/result')
             .send({
                 title: 'Melbourne'
             })
             expect(res.statusCode).toEqual(201);
-            done();
-    }), 
-    it('Route to index.html', async () => {
-        const res = await request(app)
-          .get('/')
-          .send('./dist/index.html')
-        expect(res.statusCode).toEqual(200);
-        done();
-    })
+    });
 });
-
-// afterAll(async done => {
-//     // Closing the DB connection allows Jest to exit successfully.
-//     dbConnection.close();
-//     done();
-// });
